9bb29a2fe3bcd081a9cfa95990e73a772fdaa672
Captaincrogs <2003829@talnet.nl>
2019-11-15 13:29:26 +0100

    Uitwerking van de exercise

---
+++ klaslokaal.php
@@ -0,0 +1,33 @@
+#!/usr/bin/env php
+
+<?php
+
+$array = array(
+    "foo" => "bar",
+    "bar" => "foo",
+    100   => -100,
+    -100  => 100,
+);
+echo var_dump($array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
